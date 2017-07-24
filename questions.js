0c2d2b07411913a58588d12a7fd3e0f837e4e4f0
Vicky Chen <c.vicky1213@gmail.com>
2017-07-24 04:16:22 -0400

    all qs from NY test 1

--- questions.js
+++ questions.js
@@ -7,7 +7,7 @@ module.exports = {
      */
     "QUESTIONS_EN_US" : [
         {
-            "Fill in the blank. While driving in New York State, blank is a traffic violation, punishable by a fine of $100-150 and five license points.":[
+            "Fill in the blank. While driving in New York State, blank is a traffic violation, punishable by a fine of $100-150 and five license points.": [
                 "all of the above",
                 "listening to a hand-held mobile phone",
                 "speaking into a hand-held mobile phone",
@@ -31,7 +31,7 @@ module.exports = {
             ]
         },
         {
-            "If your tire goes flat while driving, you must":[
+            "If your tire goes flat while driving, you must": [
                 "hold the steering wheel firmly",
                 "press the brakes hard",
                 "put your foot on the gas pedal",
@@ -39,7 +39,7 @@ module.exports = {
             ]
         },
         {
-            "Fiil in the blank. The risk of driving impairment begins to rise noticeably at a Blood Alcohol Concentration (BAC) of blank and increases rapidly thereafter.":[
+            "Fiil in the blank. The risk of driving impairment begins to rise noticeably at a Blood Alcohol Concentration (BAC) of blank and increases rapidly thereafter.": [
                 ".05 percent",
                 ".08 percent",
                 "1.0 percent",
@@ -47,7 +47,7 @@ module.exports = {
             ]
         },
         {
-            "Fill in the blank. When another driver passes you on the left, you must blank until the vehicle passes.":[
+            "Fill in the blank. When another driver passes you on the left, you must blank until the vehicle passes.": [
                 "slow down slightly and keep to the right",
                 "pull over and stop",
                 "slow down slightly and keep to the left",
@@ -56,7 +56,7 @@ module.exports = {
 
         },
         {
-            "A driver entering a traffic circle or rotary must yield the right-of-way to":[
+            "A driver entering a traffic circle or rotary must yield the right-of-way to": [
                 "both pedestrians and drivers already in the circle.",
                 "neither pedestrians nor drivers already in the circle.",
                 "pedestrians.",
@@ -65,7 +65,7 @@ module.exports = {
 
         },
         {
-            "You must NOT pass a vehicle from the left when":[
+            "You must NOT pass a vehicle from the left when": [
                 "your lane has a solid yellow center line.",
                 "your lane has a broken white line.",
                 "you are far from a curve.",
@@ -73,7 +73,7 @@ module.exports = {
             ]
         },
         {
-            "Two-thirds of all deer-vehicle collisions happen during the months of":[
+            "Two-thirds of all deer-vehicle collisions happen during the months of": [
                 "October, November and December",
                 "June, July and August",
                 "January, February and March",
@@ -89,7 +89,7 @@ module.exports = {
             ] 
         },
         {
-            "Fill in the blank. Blank are designed to allow vehicles to slow down and leave the expressways.":[
+            "Fill in the blank. Blank are designed to allow vehicles to slow down and leave the expressways.": [
                 "Exit ramps",
                 "Turnpikes",
                 "Acceleration lanes",
@@ -98,7 +98,7 @@ module.exports = {
 
         },
         {
-            "When you don't see a posted speed limit in New York City, you must drive at":[
+            "When you don't see a posted speed limit in New York City, you must drive at": [
                 "25 mph or less",
                 "40 mph or less",
                 "55 mph or less",
@@ -107,47 +107,140 @@ module.exports = {
         },
         {
             "When a driver tailgates you, you must": [
-                "slow down and pull off the road, if necessary"
-                "move over to the left lane"
-                "increase your speed"
+                "slow down and pull off the road, if necessary",
+                "move over to the left lane",
+                "increase your speed",
                 "apply brakes and discourage the driver"
             ]
 
         },
         {
-
+            "When making a turn, the law requires you to signal a turn with your directional lights or hand signals at least": [
+                "100 feet ahead",
+                "25 feet ahead",
+                "75 feet ahead",
+                "50 feet ahead"
+            ]
         },
         {
-
+            "Fill in the blank. For better visibility during rain, fog, and snow, you must keep your vehicle's blank on.": [
+                "low beam headlights",
+                "high beam headlights",
+                "emergency lights",
+                "all of the above"
+            ]
         },
         {
-
+            "In heavy rain, a vehicle's tires may begin to ride on the water lying on top of the road pavement when traveling at high speeds. This is known as": [
+                "hydroplaning",
+                "weaving",           
+                "waterplaning",
+                "tailgating"
+            ]
         },
         {
-
+            "When confronted by an aggressive driver while driving, avoid conflict by": [
+                "avoiding eye contact",
+                "stopping and getting out of your vehicle",
+                "using gestures",           
+                "name-calling"
+            ]
         },
         {
-
+            "If your rear wheels start to skid, you must": [
+                "turn the steering wheel in the direction of the skid.",
+                "turn the steering wheel toward the right.",
+                "turn the steering wheel toward the left.",
+                "turn the steering wheel in the opposite direction of the skid."
+            ]
         },
         {
-
+            "Fill in the blank. You must blank for an emergency vehicle, if it is coming toward you with its lights flashing, even if it's in the opposite lane of a two-way roadway.": []
+                "pull over and stop",
+                "increase the speed and clear the lane",
+                "slow down and move over to the left lane",
+                "change your lane and proceed with the same speed"
+            ]
         },
         {
-
+            "Which of the following methods is used to turn around on a narrow, two-way street?": [
+                "a three-point turn method",
+                "a four-point turn method",
+                "a single-point turn method",
+                "a two-point turn method"
+            ]
         },
         {
-
+            "What is the permitted speed limit for vehicles in roundabouts?": [
+                "30 mph or less",
+                "70 mph or less",
+                "40 mph or less",
+                "50 mph or less"
+            ]
         },
         {
-
+            "If drivers approaching from opposite directions reach the stop signs at about the same time, then": [
+                "the driver turning left must yield to the driver going straight or turning right.",
+                "the driver turning right must yield to the driver turning left",
+                "the vehicle with more passengers must go first",            
+                "the driver on the right must yield to the driver on the left"
+            ]
         },
         {
-
+            "You may not make a U-turn 500 feet in either direction from": [
+                "the crest of a hill",
+                "a construction zone",
+                "an intersection",             
+                "All of the above"
+            ]
         },
         {
-
+            "When there are double solid lines next to your lane, you are": [
+                "not allowed to pass or change lanes.",
+                "allowed to pass.",
+                "allowed to turn.",
+                "allowed to change lanes."
+            ]
         },
-
-
+         {
+            "You must NOT pass a vehicle on the right when": [
+                "the vehicle ahead is making a right turn.",
+                "oncoming vehicles are not making a left turn.",
+                "you are driving on a one-way road with two lanes of traffic.",                
+                "the vehicle ahead is going straight."
+            ]
+        },
+         {
+            "To maintain a good space cushion between your vehicle and the vehicle ahead, you should use the": [
+                "two-second rule",
+                "three-second rule",
+                "one-second rule",
+                "four-second rule"
+            ]
+        },
+         {
+            "Fill in the blank. Your headlights must be on low-beam at night when you are within blank of an approaching vehicle.": [
+                "500 feet",
+                "400 feet",                
+                "200 feet",
+                "300 feet"
+            ]
+        },
+         {
+            "Which of the following statements regarding work zones is NOT true?": [
+                "Speeding ticket fines are usually less in work zones",
+                "Diamond-shaped orange “work zone” warning signs are often posted in advance of road construction projects",
+                "Some work zones are mobile",
+                "A flagger has the same authority as a regulatory sign"
+            ]
+        },  
+         {
+            "Fill in the blank. New York State law requires every occupant blank to wear a seat belt.": [
+                "in the front seats and those under the age of 16 in the rear seats",
+                "in the rear seats",                
+                "in the front seats",
+                "in the rear seats and those under the age of 16 in the front seats"
+            ]
+        }
     ]
 }
