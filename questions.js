ea0272525b847c495f9f92a50ff80897a71a2a93
Vicky Chen <c.vicky1213@gmail.com>
2017-07-25 02:38:40 -0400

    one more

--- questions.js
+++ questions.js
@@ -179,7 +179,7 @@ module.exports = {
             "If drivers approaching from opposite directions reach the stop signs at about the same time, then": [
                 "the driver turning left must yield to the driver going straight or turning right.",
                 "the driver turning right must yield to the driver turning left",
-                "the vehicle with more passengers must go first",            
+                "the vehicle with more passengers must go first",
                 "the driver on the right must yield to the driver on the left"
             ]
         },
@@ -187,7 +187,7 @@ module.exports = {
             "You may not make a U-turn 500 feet in either direction from": [
                 "the crest of a hill",
                 "a construction zone",
-                "an intersection",             
+                "an intersection",
                 "All of the above"
             ]
         },
@@ -203,7 +203,7 @@ module.exports = {
             "You must NOT pass a vehicle on the right when": [
                 "the vehicle ahead is making a right turn.",
                 "oncoming vehicles are not making a left turn.",
-                "you are driving on a one-way road with two lanes of traffic.",                
+                "you are driving on a one-way road with two lanes of traffic.",
                 "the vehicle ahead is going straight."
             ]
         },
@@ -218,7 +218,7 @@ module.exports = {
          {
             "Fill in the blank. Your headlights must be on low-beam at night when you are within blank of an approaching vehicle.": [
                 "500 feet",
-                "400 feet",                
+                "400 feet",
                 "200 feet",
                 "300 feet"
             ]
@@ -234,10 +234,266 @@ module.exports = {
          {
             "Fill in the blank. New York State law requires every occupant blank to wear a seat belt.": [
                 "in the front seats and those under the age of 16 in the rear seats",
-                "in the rear seats",                
+                "in the rear seats",
                 "in the front seats",
                 "in the rear seats and those under the age of 16 in the front seats"
             ]
-        }        
+        },
+        {
+            "When a school bus has red lights flashing on a divided highway, you must": [
+                "stop if you are driving either direction.",
+                "stop only if you are traveling the same direction as the bus.",
+                "slow down and prepare to stop if you can see children.",
+                "stop only if you are traveling the opposite direction of the bus."
+            ]
+        },
+        {
+            "Fill in the blankc. Unless posted, the maximum speed is blank mph.": [
+                "55",
+                "70",
+                "50",
+                "65"
+            ]
+        },
+        {
+            "Fill in the blank. After you stop at a red arrow and yield to pedestrians, you may turn blank while the light is still red.": [
+                "You are never permitted to turn on a red arrow.",
+                "right or left",
+                "right",
+                "left"
+            ]
+        },
+        {
+            "Changing lanes does NOT include": [
+                "turning from one road to another.",
+                "moving from one lane to another.",
+                "entering the freeway from an on-ramp.",
+                "entering the road from the shoulder."
+            ]
+        },  
+        {
+            "As you approach an intersection, you notice a bicycle to your left has arrived before you. Who is given right-of-way?": [
+                "The bicycle, because it arrived first.",
+                "The bicycle, because it is to your left.",
+                "You, because bicycles must always yield right-of-way to cars.",
+                "You, because you are to the right of the bicycle."
+            ]
+        },  
+        {
+            "Lanes marked with a diamond symbol are": [
+                "high-occupancy vehicle lanes.",
+                "exit ramps.",
+                "turning-only lanes.",
+                "emergency vehicle lanes."
+            ]
+        },  
+        {
+            "Before changing lanes, you should do all of the following EXCEPT": [
+                "flash your headlights to alert other drivers.",
+                "look for vehicles in your blind spot.",
+                "glance over your shoulder to make sure the lane is clear.",
+                "be sure there is enough room for your vehicle in the next lane."
+            ]
+        },  
+        {
+            "In New York state, when can a left turn be made against a red light?": [
+                "When turning from a one-way street to another one-way street",
+                "When turning from a one-lane to a two-lane road",
+                "Only during posted time frames",
+                "Left turns can never be made against a red light"                
+            ]
+        },  
+        {
+            "When turning left from a one-way street to a two-way street, you should begin your turn in": [
+                "the left lane.",
+                "any lane.",                
+                "the middle lane.",
+                "the right lane."
+            ]
+        },  
+        {
+            "An intersection where the traffic control lights are not working is treated": [
+                "like a four-way stop.",
+                "so that vehicles on the larger of the two roads have the right-of-way.",
+                "as if it is blocked off. Choose another route for travel.",
+                "as though there is a flashing yellow light."
+            ]
+        },  
+        {
+            "Turning right on a red light in New York City is": [
+                "prohibited unless a sign is posted.",
+                "permitted on multi-lane roads only.",
+                "prohibited at all times.",
+                "permitted at all times"
+            ]
+        },  
+        {
+            "When approaching a flashing yellow light at an intersection, you should": [
+                "slow down and proceed with caution.",
+                "stop and proceed when it is safe.",
+                "prepare to stop, the red signal is about to appear.",
+                "stop and remain stopped until a green light appears."
+            ]
+        },  
+        {
+            "If you look at your steering wheel as a clock, what is the best hand position?": [
+                "9 o'clock and 3 o'clock",
+                "8 o'clock and 4 o'clock",
+                "10 o'clock and 2 o'clock",
+                "9 o'clock and 4 o'clock"
+            ]
+        },  
+        {
+            "When parked facing uphill on the right side of the road with a curb, you should steer": [
+                "away from the curb.",
+                "straight.",
+                "towards the curb.",
+                "towards the side with the least amount of vehicles."
+            ]
+        },  
+        {
+            "Fill in the blank. If parked near a fire hydrant, you must be at least blank feet from the fire hydrant.": [
+                "15",
+                "10",
+                "20",
+                "5"
+            ]
+        },  
+        {
+            "When in a roundabout, you should do all of the following EXCEPT": [
+                "stop your vehicle to turn.",
+                "stay in your lane until ready to exit.",
+                "signal before exiting.",
+                "yield right-of-way to pedestrians."
+            ]
+        },  
+        {
+            "Construction zones usually have": [
+                "increased speed limits.",
+                "reduced speed limits.",
+                "no speed limits.",
+                "a standard speed limit of 15 mph."
+            ]
+        },  
+        {
+            "In which of the following places is a U-turn permitted?": [
+                "Across a double yellow line",
+                "On a railroad crossing",
+                "In a business district.",
+                "On a one-way street"
+            ]
+        },  
+        {
+            "You are approaching an intersection. A truck reaches the intersection to your right at the same time. Who is given right-of-way?": [
+                "The truck",
+                "Whoever moves first",
+                "You",
+                "The larger of the two vehicles"
+            ]
+        },  
+        {
+            "When parked alongside the curb, how close should your vehicle be to the curb?": [
+                "1 foot",
+                "2 feet",
+                "3 feet",
+                "18 inches"
+            ]
+        },  
+        {
+            "When driving on a two-lane road with a solid yellow center line and a broken yellow center line, are you permitted to pass a vehicle in front of you?": [
+                "No, passing is not permitted.",
+                "Yes, if the broken line is on your side.",
+                "Yes, if the solid line is on your side.",
+                "Yes, regardless of which line is on your side."
+            ]
+        },  
+        {
+            "Fill in the blank. Traffic moves around a roundabout in blank.": [
+                "a counter-clockwise direction",
+                "a clockwise direction",
+                "both directions",      
+                "the direction indication by traffic lights"
+            ]
+        },  
+        {
+            "Before every lane change, you should": [
+                "all of the above",
+                "check your blind spots.",
+                "check your mirrors.",
+                "signal your intentions",
+             ]   
+        },  
+        {
+            "You should complete a right turn in": [
+                "the right lane.",
+                "whichever lane is best for your trip plan.",
+                "the left lane.",
+                "whichever lane interferes least with traffic."
+            ]
+        },  
+        {
+            "When are you permitted to drive to the left of two solid yellow lines?": [
+                "All of the above",
+                "When turning left to leave the highway.",
+                "When instructed to do so by traffic officers.",
+                "When performing a U-turn"
+            ]   
+        },  
+        {
+            "Fiil in the blank. In the state of New York (except New York City), when turning on a two-way road, after you stop at a solid red light and yield to pedestrians and oncoming traffic, you may turn blank while the light is still red.": [
+                "right",
+                "left",
+                "left or right",
+                "You are never permitted to turn on a red light."
+            ]
+        },  
+        {
+            "Lines that separate two or more lanes of traffic traveling in the same direction are": [
+                "solid or broken white lines.",
+                "solid yellow lines.",
+                "broken yellow lines.",
+                "double yellow lines."
+            ]
+        },  
+        {
+            "If parked on a level street, in which directions should your tires point?": [
+                "Parallel to the curb",
+                "Towards the curb",
+                "Away from the curb",
+                "Always to the right."
+            ]
+        },  
+        {
+            "If your signal lights are broken and you would like to signal that you are turning left, what is the correct hand-and-arm signal?": [
+                "Left arm straight out the window.",
+                "Left arm bent at 90 degrees pointing up.",
+                "Left arm bent at 90 degrees pointing down.",
+                "Right arm straight out the window."
+            ]
+        },  
+        {
+            "Borrowing a vehicle plate for the disabled from a friend who is not with you is permitted only" : [
+                "Borrowing a license plate is never permitted.",
+                "if you are driving their vehicle as well.",
+                "if the disabled friend will not be using it that day.",
+                "in emergency situations."
+            ]
+        },  
+        {
+            "Fill in the blank. You must not park within blank feet of a railroad track.": [
+                "50",
+                "30",
+                "20",
+                "10"
+            ]
+        },  
+        {
+            "You may go around lowered railroad gates": [
+                "You should never go around lowered railway gates.",
+                "For light rail transit vehicle crossings only.",
+                "For railroad crossings only.",
+                "Only when you can see the train has passed."
+            ]
+        }  
     ]
 }
